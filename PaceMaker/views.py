48274aacfcb5f67c80b8fed697cfa65d89aa094e
이세훈 <hoi125@naver.com>
2018-05-24 23:46:04 +0900

    지우세요 로그인 리콰이어드

--- PaceMaker/views.py
+++ PaceMaker/views.py
@@ -2,15 +2,8 @@ from django.views.generic.base import TemplateView
 
 # login_required()함수는 데코레이터로 사용되는 함수로, 일반 함수에 적용
 # 사용자가 로그인 했는지를 확인해 로그인한 경우는 원래 함수로 실행하고, 로그인 되지 않은 경우는 로그인 페이지로 리다이렉트.
-from django.contrib.auth.decorators import login_required
-
 
 #--- TemplateView
 class HomeView(TemplateView):
     template_name = 'index.html'
 
-class LoginRequiredMixin(object):
-    @classmethod
-    def as_view(cls, **initkwargs):
-        view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
-        return login_required(view)
