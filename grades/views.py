5b884140a818b8da5eece23be0e9bc997788824d
jihoon <blesk0111@gmail.com>
2018-05-30 16:21:55 +0900

    MSC 과목 메뉴 & 총 설계학점 확인 추가

--- grades/views.py
+++ grades/views.py
@@ -10,7 +10,6 @@ import requests
 from dataParser import parser
 # Create your views here.
 
-
 def getIntScore(self, score):
     if score == 'A+':
         return 4.5
@@ -42,12 +41,12 @@ class GradeLV(LoginRequiredMixin,ListView):
     def get_score_sum(self):
         s = self.request.user.hukbun
         sum = 0
-        scorelist = StudentGrade.objects.filter(hukbun=s).filter(valid = '유효').values_list('score', flat=True)
+        scorelist = StudentGrade.objects.filter(hukbun=s).filter(valid = '유효').exclude(grade='P').values_list('score', flat=True)
+        print(scorelist)
         for i in scorelist:
-            sum = sum + i
+            sum = sum + int(i)
 
         return sum
-
 
     def get_avgGrade(self):
         s = self.request.user.hukbun
@@ -73,9 +72,21 @@ class GradeLV(LoginRequiredMixin,ListView):
         fake_grade_sum = StudentGrade.objects.filter(hukbun=s).values_list('score', flat=True)
         sum = 0
         for i in fake_grade_sum:
-            sum = sum + i
+            sum = sum + int(i)
 
         return sum
+
+    def get_design_grade_sum(self):
+        s = self.request.user.hukbun
+        design_gradelist = StudentGrade.objects.filter(hukbun=s).values_list('grade_design', flat=True)
+        sum = 0.0
+        for i in design_gradelist:
+            sum = sum + int(i)
+
+        return sum
+
+
+
 
     def get_context_data(self,**kwargs):
         context = super(GradeLV, self).get_context_data(**kwargs)
@@ -83,6 +94,7 @@ class GradeLV(LoginRequiredMixin,ListView):
         context['grade_sum'] = self.get_score_sum()
         context['avgGrade'] = self.get_avgGrade()
         context['fake_grade_sum'] = self.get_fake_grade_sum()
+        context['design_grade_sum'] = self.get_design_grade_sum()
         return context
 
 
@@ -96,7 +108,7 @@ class MajorGradeLV(LoginRequiredMixin,ListView):
         sum = 0
         scorelist = StudentGrade.objects.filter(hukbun=s).filter(Q(eisu='컴과') | Q(eisu='전필')).filter(valid = '유효').values_list('score', flat=True)
         for i in scorelist:
-            sum = sum + i
+            sum = sum + int(i)
 
         return sum
 
@@ -120,7 +132,7 @@ class MajorGradeLV(LoginRequiredMixin,ListView):
         fake_grade_sum = StudentGrade.objects.filter(hukbun=s).filter(Q(eisu='컴과') | Q(eisu='전필')).values_list('score', flat=True)
         sum = 0
         for i in fake_grade_sum:
-            sum = sum + i
+            sum = sum + int(i)
 
         return sum
 
@@ -137,6 +149,7 @@ class MajorGradeLV(LoginRequiredMixin,ListView):
         context['fake_grade_sum'] = self.get_fake_grade_sum()
         return context
 
+
 class GeGradeLV(LoginRequiredMixin,ListView):
     model = StudentGrade
     template_name = 'grades/ge_subject_list.html'
@@ -151,7 +164,7 @@ class GeGradeLV(LoginRequiredMixin,ListView):
         sum = 0
         scorelist = StudentGrade.objects.filter(hukbun=s).filter(Q(eisu='M자') | Q(eisu='필수') | Q(eisu='역철') | Q(eisu='경사') | Q(eisu='체기') | Q(eisu='사회')).filter(valid = '유효').values_list('score', flat=True)
         for i in scorelist:
-            sum = sum + i
+            sum = sum + int(i)
 
         return sum
 
@@ -175,13 +188,64 @@ class GeGradeLV(LoginRequiredMixin,ListView):
         fake_grade_sum = StudentGrade.objects.filter(hukbun=s).filter(Q(eisu='M자') | Q(eisu='필수') | Q(eisu='역철') | Q(eisu='경사') | Q(eisu='체기') | Q(eisu='사회')).values_list('score', flat=True)
         sum = 0
         for i in fake_grade_sum:
-            sum = sum + i
+            sum = sum + int(i)
 
         return sum
 
     def get_context_data(self,**kwargs):
         context = super(GeGradeLV, self).get_context_data(**kwargs)
         context['ge_list'] = self.get_queryset()
+        context['grade_sum'] = self.get_score_sum()
+        context['avgGrade'] = self.get_avgGrade()
+        context['fake_grade_sum'] = self.get_fake_grade_sum()
+        return context
+
+
+class MscGradeLV(LoginRequiredMixin,ListView):
+    model = StudentGrade
+    template_name = 'grades/msc_subject_list.html'
+    context_object_name = 'msc_list'
+
+    def get_queryset(self):
+        test = self.request.user.hukbun
+        return StudentGrade.objects.filter(hukbun=test).filter(eisu='M자')
+
+    def get_score_sum(self):
+        s = self.request.user.hukbun
+        sum = 0
+        scorelist = StudentGrade.objects.filter(hukbun=s).filter(eisu='M자').values_list('score', flat=True)
+        for i in scorelist:
+            sum = sum + int(i)
+
+        return sum
+
+    def get_avgGrade(self):
+        s = self.request.user.hukbun
+        avg = 0.0
+        sum = 0.0
+        gradelist = StudentGrade.objects.filter(hukbun=s).filter(eisu='M자').filter(Q(grade='A+') | Q(grade='A') | Q(grade='B+') | Q(grade='B') | Q(grade='C+') | Q(grade='C') | Q(grade='D+') | Q(grade='D') | Q(grade='F')).values_list('grade', flat=True)
+        count = len(gradelist)
+        for g in gradelist:
+            s = getIntScore(self, g)
+            if s == None:
+                continue
+            else:
+                sum = sum + s
+        avg = sum / count
+        return avg
+
+    def get_fake_grade_sum(self):
+        s = self.request.user.hukbun
+        fake_grade_sum = StudentGrade.objects.filter(hukbun=s).filter(eisu='M자').values_list('score', flat=True)
+        sum = 0
+        for i in fake_grade_sum:
+            sum = sum + int(i)
+
+        return sum
+
+    def get_context_data(self, **kwargs):
+        context = super(MscGradeLV, self).get_context_data(**kwargs)
+        context['msc_list'] = self.get_queryset()
         context['grade_sum'] = self.get_score_sum()
         context['avgGrade'] = self.get_avgGrade()
         context['fake_grade_sum'] = self.get_fake_grade_sum()
