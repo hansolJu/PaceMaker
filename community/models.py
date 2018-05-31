3a7d9433cee17486f712bda4cf9fda0c7ba40f8c
이세훈 <hoi125@naver.com>
2018-05-31 21:57:29 +0900

    게시판 1프로 남음

--- community/models.py
+++ community/models.py
@@ -4,8 +4,7 @@ from tagging.fields import TagField
 from dataParser.models import StudentInfo
 from django.utils.text import slugify
 
-
-class Post(models.Model):
+class PostIF(models.Model):
     title =  models.CharField('제목', max_length=50)
     slug = models.SlugField('슬러그', unique=True, allow_unicode=True, help_text='one word for title alias.')
     description = models.CharField('요약 설명', max_length=100, blank=True,
@@ -15,21 +14,22 @@ class Post(models.Model):
     create_date = models.DateTimeField('생성 날짜', auto_now_add=True)
     modify_date = models.DateTimeField('수정 날짜', auto_now=True)
     owner = models.ForeignKey(StudentInfo, on_delete=models.CASCADE)
-    hits = models.IntegerField('조회수',null=True, blank=True)
+    file = models.FileField('파일',blank=True, upload_to='documents/%Y/%m/%d')
+    hits = models.IntegerField('조회수',  blank=True)
 #tagField는 CharField를 상속받아서 디폴트로 amx_length=255,Blank=True로 정의하고 있어서 따로 내용을 안채워도됨
 
 
     class Meta:
         verbose_name = 'post'
         verbose_name_plural = 'posts'
-        db_table = 'my_post'
-        ordering = ('-modify_date',)
+        db_table = 'post_if'
+        ordering = ('-pk',)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('community:post_detail', args=(self.slug,))
+        return reverse('community:Info_detail', args=(self.slug,))
 
     def get_previous_post(self):
         return self.get_previous_by_modify_date()
@@ -40,5 +40,43 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         if not self.id:
             self.slug = slugify(self.title, allow_unicode=True)
-        super(Post, self).save(*args, **kwargs)
+        super(PostIF, self).save(*args, **kwargs)
+
+class PostOB(models.Model):
+    title =  models.CharField('제목', max_length=50)
+    slug = models.SlugField('슬러그', unique=True, allow_unicode=True, help_text='one word for title alias.')
+    description = models.CharField('요약 설명', max_length=100, blank=True,
+                                   help_text='simple description text.')
+    content = models.TextField('내용')
+    tag = TagField('태그')
+    create_date = models.DateTimeField('생성 날짜', auto_now_add=True)
+    modify_date = models.DateTimeField('수정 날짜', auto_now=True)
+    owner = models.ForeignKey(StudentInfo, on_delete=models.CASCADE)
+    file = models.FileField('파일',blank=True, upload_to='documents/%Y/%m/%d')
+    hits = models.IntegerField('조회수',  blank=True)
+#tagField는 CharField를 상속받아서 디폴트로 amx_length=255,Blank=True로 정의하고 있어서 따로 내용을 안채워도됨
+
+
+    class Meta:
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+        db_table = 'post_ob'
+        ordering = ('-pk',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('community:ob_detail', args=(self.slug,))
+
+    def get_previous_post(self):
+        return self.get_previous_by_modify_date()
+
+    def get_next_post(self):
+        return self.get_next_by_modify_date()
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title, allow_unicode=True)
+        super(PostOB, self).save(*args, **kwargs)
 # Create your models here.
--- community/views.py
+++ community/views.py
@@ -1,31 +1,60 @@
 from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from django.shortcuts import render
 from tagging.views import TaggedObjectList
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from community.models import Post
+from community.models import PostIF,PostOB
 from dataParser.models import StudentInfo
+
+from django.views.generic.edit import FormView
+from community.forms import PostSearchForm,DocumentForm
+from django.db.models import Q
+from django.shortcuts import render
 
 # --- TemplateView
 class TagTV(TemplateView):
     template_name = 'tagging/tagging_cloud.html'
 
 # --- ListView
-class PostLV(ListView):
-    model = Post
-    template_name =  'community/post_all.html'
+class OldbookLV(ListView):
+    model = PostOB
+    template_name = 'community/post_oldbook.html'
     context_object_name = 'posts'
 
-    count_hit =True
-
-    paginate_by = 5
+    paginate_by = 5  # Display 5 objects per page
 
     def get_context_data(self, **kwargs):
-        context = super(PostLV, self).get_context_data(**kwargs)
+        context = super(OldbookLV, self).get_context_data(**kwargs)
         paginator = context['paginator']
-        page_numbers_range = 5
+        context['totalCnt'] = PostOB.objects.all().count()
+        page_numbers_range = 5  # Display only 5 page numbers
+        max_index = len(paginator.page_range)
+
+        page = self.request.GET.get('page')
+        current_page = int(page) if page else 1
+
+        start_index = int((current_page - 1) / page_numbers_range) * page_numbers_range
+        end_index = start_index + page_numbers_range
+        if end_index >= max_index:
+            end_index = max_index
+
+        page_range = paginator.page_range[start_index:end_index]
+        context['page_range'] = page_range
+        return context
+
+class InfoLV(ListView):
+    model = PostIF
+    template_name = 'community/post_Info.html'
+    context_object_name = 'posts'
+
+    paginate_by = 5 # Display 5 objects per page
+
+    def get_context_data(self, **kwargs):
+        context = super(InfoLV, self).get_context_data(**kwargs)
+        paginator = context['paginator']
+        context['totalCnt'] = PostIF.objects.all().count()
+        page_numbers_range = 5 # Display only 5 page numbers
         max_index = len(paginator.page_range)
 
         page = self.request.GET.get('page')
@@ -41,45 +70,149 @@ class PostLV(ListView):
         return context
 
 class PostTOL(TaggedObjectList):
-    model = Post
+    model = PostIF
     template_name = 'tagging/tagging_post_list.html'
-
 
 # --- DeatilView
 class PostDV(LoginRequiredMixin,DetailView):
-    model = Post
-    template_name = 'community/post_detail.html'
+    model = PostIF
+    template_name = 'community/Information_detail.html'
 
-    # def pageCounter(self, pk):
+    def get_queryset(self):
+        return PostIF.objects.all()    #filter(owner=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDV, self).get_context_data(**kwargs)
+        obj = self.get_object()
+        obj.hits = obj.hits + 1
+        obj.save()
+        return context
+
+
+    # def count(self, **kwargs):
+    #     context = super(PostDV, self).get_context_data(**kwargs)
+    #     hit = context['hit']
+
+    # def pageCounter(self,request):
+    #     pk = request.GET['title']
     #     post = Post.objects.get(id=pk)
-    #     post.hits += 1
-    #     post.save()
-    #     return render('community/post_detail.hmtl', {'post': post})
+    #
+    #     # 조회수를 늘린다.
+    #     Post.objects.filter(id=pk).update(hits=Post.hits + 1)
+    #
+    #     return render('community/post_Info.html', {'post':post})
 
+        # post = Post.objects.get(id=pk)
+        # post.hits += 1
+        # post.save()
+        # return redirect('community/Information_detail.hmtl', {'post': post})
 
+class OBPostDV(LoginRequiredMixin,DetailView):
+    model = PostOB
+    template_name = 'community/Oldbook_detail.html'
+
+    def get_queryset(self):
+        return PostOB.objects.all()    #filter(owner=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super(OBPostDV, self).get_context_data(**kwargs)
+        obj = self.get_object()
+        obj.hits = obj.hits + 1
+        obj.save()
+        return context
+
+class SearchFormView(InfoLV,FormView):
+    form_class = PostSearchForm
+    template_name = 'community/post_Info.html'
+
+    def form_valid(self, form):
+        schWord = '%s' %self.request.POST['search_word']
+        post_info_list = PostIF.objects.filter(Q(title__icontains=schWord)|Q(description__icontains=schWord)
+                                        |Q(content__icontains=schWord)).distinct()
+
+        context = {}
+        context['form'] = form
+        context['search_term'] = schWord
+        context['object_list'] = post_info_list
+
+        return render(self.request, self.template_name, context)
+
+class OBSearchFormView(OldbookLV,FormView):
+    form_class = PostSearchForm
+    template_name = 'community/post_oldbook.html'
+
+    def form_valid(self, form):
+        schWord = '%s' %self.request.POST['search_word']
+        post_oldbook_list = PostOB.objects.filter(Q(title__icontains=schWord)|Q(description__icontains=schWord)
+                                        |Q(content__icontains=schWord)).distinct()
+
+        context = {}
+        context['form'] = form
+        context['search_term'] = schWord
+        context['object_list'] = post_oldbook_list
+
+        return render(self.request, self.template_name, context)
 
 # --- CRUD View
 class PostCreateView(LoginRequiredMixin, CreateView):
-    model = Post
-    fields = ['title',  'description', 'content', 'tag']
+    model = PostIF
+
+    fields = ['title',  'description', 'content','hits', 'tag',  'file']
     initial = {'slug': 'auto-filling-do-not-input'}
     # slug 필드를 처리하는 또 다른 방법은 fields속성에서 제외해 폼에 나타나지 않도록 하는 방법입니다. \
     # 폼에는 보이지 않지만, Post 모델의 save()함수에 의해 테리블의 레코드에는 자동으로 채워집니다.
     # fields = ['title', 'description', 'content', 'tag']
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('community:Info_list')
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super(PostCreateView, self).form_valid(form)
 
+class OBPostCreateView(LoginRequiredMixin, CreateView):
+    model = PostOB
+
+    fields = ['title',  'description', 'content','hits', 'tag',  'file']
+    initial = {'slug': 'auto-filling-do-not-input'}
+    # slug 필드를 처리하는 또 다른 방법은 fields속성에서 제외해 폼에 나타나지 않도록 하는 방법입니다. \
+    # 폼에는 보이지 않지만, Post 모델의 save()함수에 의해 테리블의 레코드에는 자동으로 채워집니다.
+    # fields = ['title', 'description', 'content', 'tag']
+    success_url = reverse_lazy('community:ob_list')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super(OBPostCreateView, self).form_valid(form)
+
 
 class PostUpdateView(LoginRequiredMixin, UpdateView):
-    model = Post
-    fields = ['title', 'description', 'content', 'tag']
-    success_url = reverse_lazy('community:post_list')
+    model = PostIF
+    fields = ['title', 'description', 'content', 'tag','file']
+    success_url = reverse_lazy('community:Info_list')
+
+    def get_queryset(self):
+        return PostIF.objects.filter(owner=self.request.user)
+
+class OBPostUpdateView(LoginRequiredMixin, UpdateView):
+    model = PostOB
+    fields = ['title', 'description', 'content', 'tag','file']
+    success_url = reverse_lazy('community:ob_list')
+
+    def get_queryset(self):
+        return PostOB.objects.filter(owner=self.request.user)
 
 
 class PostDeleteView(LoginRequiredMixin, DeleteView):
-    model = Post
-    success_url = reverse_lazy('community:post_list')
+    model = PostIF
+    success_url = reverse_lazy('community:Info_list')
+
+    def get_queryset(self):
+        return PostIF.objects.filter(owner=self.request.user)
+
+class OBPostDeleteView(LoginRequiredMixin, DeleteView):
+    model = PostOB
+    success_url = reverse_lazy('community:ob_list')
+
+    def get_queryset(self):
+        return PostOB.objects.filter(owner=self.request.user)
+
 # Create your views here.
+
