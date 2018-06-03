fb14cbd15c332158015871cb7afa77e53f28d2cc
jihoon <blesk0111@gmail.com>
2018-06-04 01:32:17 +0900

    grade view.py 왠지 안될거 같은데 로컬에서는 돌아가길래 올림

--- grades/templatetags/index.py
+++ grades/templatetags/index.py
@@ -2,6 +2,16 @@ from django import template
 register = template.Library()
 
 
+@register.filter()
+def to_int(value):
+    return int(value)
+
+
+@register.filter
+def subtract(value, arg):
+    return value - arg
+
+
 @register.filter
 def index(List, i):
     return List[int(i)]
