193f26f9853698f694e4a603b53076aa7f7ee29f
hansol <hansol1108@naver.com>
2018-06-07 07:49:33 +0900

    디비에 저장할때 양쪽 공백 제거 .strip() 추가

--- completion/views.py
+++ completion/views.py
@@ -1,5 +1,4 @@
-# from django.core.serializers import json
-import json
+from datetime import datetime
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, JsonResponse
@@ -10,7 +9,7 @@ from django.template import loader
 from django.views import View
 from django.views.generic import TemplateView
 
-from dataParser.models import StudentGrade, StudentInfo
+from dataParser.models import StudentGrade, StudentInfo, Course
 
 
 def convert_year_semester(year_semester):
@@ -34,6 +33,15 @@ def convert_year_semester(year_semester):
         return None
 
 
+# 이수 체계도를 가져오지말고 작년에 열렸던 전공 과목을 가져온다.
+def get_lastyear_course_list():
+    present = datetime.today().year
+    last = present - 1
+    last_list = Course.objects.filter(year=last).filter(semester=10).values_list('subjectName', flat=True).distinct()
+    print(last_list)
+
+
+last_year_course_list = []
 eisu_2012_list = [
     # 1-1 1
     ['창의기초설계', '컴퓨터과학전공및진로탐색'],
@@ -140,21 +148,24 @@ class DiagramTV(LoginRequiredMixin, TemplateView):
                             e_semester.remove(e_subject)
         return my_eisu
 
-    # 과목명을 입력하면 그과목의 학점을 리턴 (성적에서)
-    def get_course_score(self, course_name):
+    # 과목명을 입력하면 그과목의 학점을 리턴 (성적 디비에서)
+    def get_grade_score(self, course_name):
         s = self.get_user_id()
-        course_score = StudentGrade.objects \
-            .filter(hukbun=s) \
-            .filter(valid='유효') \
-            .filter(subject=course_name) \
-            .values_list('score', flat=True)
-        for i in course_score:
-            course_score = i
-        print(course_score)
+        try:
+            course_score = StudentGrade.objects \
+                .filter(hukbun=s) \
+                .filter(valid='유효') \
+                .filter(subject=course_name) \
+                .values_list('score', flat=True)[0]
+        except:
+            course_score = '0'
+        # print(course_score)
+        # for i in course_score:
+        #     course_score = i
+        # print(course_score)
         return course_score
 
-    #
-    # 과목명을 입력하면 그과목의 설계 학점을 리턴
+    # 과목명을 입력하면 그과목의 설계 학점을 리턴(Grade 테이블에서)  --> course는 디비 테이블이 아니라 과목
     def get_course_grade_design(self, course_name):
         s = self.get_user_id()
         course_grade_design = StudentGrade.objects \
@@ -164,8 +175,50 @@ class DiagramTV(LoginRequiredMixin, TemplateView):
             .values_list('grade_design', flat=True)
         for i in course_grade_design:
             course_grade_design = i
-        print(course_grade_design)
+        # print(course_grade_design)
         return course_grade_design
+
+    # 과목명을 입력하면 그과목의 학점을 리턴(course 테이블에서)
+    def get_f_course_t_subj_score(self, course_name):
+        s = self.get_user_id()
+        year = int(s[0:4])
+        if year < 2012:
+            year = 2012 - 1
+        elif 2011 < year < 2016:
+            year = 2012 - 1
+        elif 2015 < year < 2017:
+            year = 2016 - 1
+        elif 2016 < year < 2018:
+            year = 2017 - 1
+        else:
+            year = 2018 - 1
+        subject_score = Course.objects \
+            .filter(year__gte=year) \
+            .filter(subjectName=course_name) \
+            .distinct() \
+            .values_list('score', flat=True)[0]
+        return subject_score
+
+    # 과목명을 입력하면 그과목의 설계 학점을 리턴(Course 테이블에서)  --> course는 디비 테이블이 아니라 과목
+    def get_f_course_t_subj_design(self, course_name):
+        s = self.get_user_id()
+        year = int(s[0:4])
+        if year < 2012:
+            year = 2012 - 1
+        elif 2011 < year < 2016:
+            year = 2012 - 1
+        elif 2015 < year < 2017:
+            year = 2016 - 1
+        elif 2016 < year < 2018:
+            year = 2017 - 1
+        else:
+            year = 2018 - 1
+        subject_design = Course.objects \
+            .filter(year__gte=year) \
+            .filter(subjectName=course_name) \
+            .distinct() \
+            .values_list('design_score', flat=True)[0]
+        return subject_design
 
     # 과목명을 입력하면 그과목의 이수구분을 리턴
     # ex> 대학영어1--> 교양, 미분적분학1---> msc, C프로그래밍--> 전공
@@ -178,15 +231,25 @@ class DiagramTV(LoginRequiredMixin, TemplateView):
             .values_list('eisu', flat=True)
         for i in course_eisu:
             course_eisu = i
-        print(course_eisu)
         if course_eisu == '컴과' or course_eisu == '전필':
-            print("전공")
             return "전공"
-        elif course_eisu == 'M자':
-            print("MSC")
+        elif course_eisu == 'M자' or course_eisu == '수리' or course_eisu == '이계':
             return "MSC"
         else:
-            print("교양")
+            return "교양"
+
+    def get_course_eisu_form_course(self, course_name):
+        s = self.get_user_id()
+        course_eisu = Course.objects \
+            .filter(subjectName=course_name) \
+            .values_list('eisu', flat=True).distinct()
+        for i in course_eisu:
+            course_eisu = i
+        if course_eisu == '컴과' or course_eisu == '전필':
+            return "전공"
+        elif course_eisu == 'M자' or course_eisu == '수리' or course_eisu == '이계':
+            return "MSC"
+        else:
             return "교양"
 
     # 내가 들은 과목을 과목당 (학점/설계학점)으로 묶음 ex> [대학영어1,3,0]
@@ -197,7 +260,7 @@ class DiagramTV(LoginRequiredMixin, TemplateView):
             semester_tmp = []
             for subject in semester:
                 subject_tmp = []
-                this_score = self.get_course_score(subject)
+                this_score = self.get_grade_score(subject)
                 this_design = self.get_course_grade_design(subject)
                 subject_tmp.append(subject)
                 subject_tmp.append(this_score)
@@ -207,20 +270,60 @@ class DiagramTV(LoginRequiredMixin, TemplateView):
         print(result)
         return result
 
-    # 이수체계도에서 들은 과목을 제거하고 남은 리스트 반납한 과목당 (학점/수업시간/설계학점)으로 묶음
+    # 이수체계도에서 들은 과목을 제거하고 남은 리스트 반납한 과목당 [이름,학점,설계학점,이수구분]으로 묶음
     def set_course_custom_eisu_list(self):
         result = []
-        eisu_list = self.set_course_custom_eisu_list()
+        eisu_list = self.custom_eisu()
+        # print(eisu_list)
         for semester in eisu_list:
             semester_tmp = []
             for subject in semester:
                 subject_tmp = []
-                this_score = self.get_course_score(subject)
-                this_design = self.get_course_grade_design()
-
-        pass
+                print(subject)
+                this_eisu = self.get_course_eisu_form_course(subject)
+                this_score = self.get_f_course_t_subj_score(subject)
+                this_design = self.get_f_course_t_subj_design(subject)
+                subject_tmp.append(subject)
+                subject_tmp.append(this_score)
+                subject_tmp.append(this_design)
+                subject_tmp.append(this_eisu)
+                semester_tmp.append(subject_tmp)
+            result.append(semester_tmp)
+        for i in result:
+            print(i)
+        return result
 
     # 내가 들은 과목을 제외한 나머지 학점을 계산
     def set_course_score(self):
-        pass
+        result =[]
+        my_list = self.get_course_taken()
+        total_grade = 136
+        major_grade = 67
+        gyoyang_grade = 47
+        msc_grade = 24
+        design_grade = 12
+        for semester in my_list:
+            for subject in semester:
+                eisu = self.get_course_eisu(subject)
+                score = int(self.get_grade_score(subject))
+                design = self.get_course_grade_design(subject)
+                print(subject, eisu, score, design)
+                total_grade = total_grade - score
+                if eisu == "전공":
+                    major_grade = major_grade - score
+                    design_grade = design_grade - float(design)
+                elif eisu == "교양":
+                    gyoyang_grade = gyoyang_grade - score
+                elif eisu == "MSC":
+                    msc_grade = msc_grade - score
+                    gyoyang_grade = gyoyang_grade - score
+        print(total_grade,major_grade,gyoyang_grade,msc_grade,design_grade)
+        result.append(total_grade)
+        result.append(major_grade)
+        result.append(gyoyang_grade)
+        result.append(msc_grade)
+        result.append(design_grade)
+        return result
 
+    # def get(self, request, *args, **kwargs):
+    #     return HttpResponse(self.set_course_score())
